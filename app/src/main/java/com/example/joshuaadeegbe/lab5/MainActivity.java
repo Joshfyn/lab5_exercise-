7753448974e7d3b7d531381c9c21e0639e6831e4
Joshfyn <joshuaadeegbe@yahoo.com>
2018-02-21 21:27:42 +0200

    Test

--- app/src/main/java/com/example/joshuaadeegbe/lab5/MainActivity.java
+++ app/src/main/java/com/example/joshuaadeegbe/lab5/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.joshuaadeegbe.lab5;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        String[] items = {"red", "blue","green",
+                "red", "blue","green",
+                "red", "blue","green",
+                "red", "blue","green",
+                "red", "blue","green",
+                "red", "blue","green",
+                "red", "blue","green",
+                "red", "blue","green"};
+
+        ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);     //this object converts an array to list items
+
+        listView = (ListView) findViewById(R.id.my_listview);
+
+        listView.setAdapter(listAdapter); //this coverts the array to display in the list
+
+
     }
 }
